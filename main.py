5e994456aad26d42c269342b59337cf4d40c1f91
dake <elessarelfstone@mail.ru>
2017-12-12 09:35:49 +0500

    some changes

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import bussnavparser
 import dbfill
 
 
-max_page = 3
+if __name__ == "__main__":
+	max_page = 3
 
-#
-parser = bussnavparser.BusinessNavigatorParser()
-# print(data[0])
-#data = parser.get_data_by_pagin_page(2)
-#print(data[0])
-data = parser.get_data_by_pagin_range(1,3)
-tstdb = dbfill.DbFill("database.ini")
-tst = tstdb.fill_main_storage(parser.get_structure(), data, "iso-8859-5")
+	#
+	parser = bussnavparser.BusinessNavigatorParser()
+	# print(data[0])
+	#data = parser.get_data_by_pagin_page(2)
+	#print(data[0])
+	data = parser.get_data_by_pagin_range(1,3)
+	tstdb = dbfill.DbFill("database.ini")
+	tst = tstdb.fill_main_storage(parser.get_structure(), data, "iso-8859-5")
